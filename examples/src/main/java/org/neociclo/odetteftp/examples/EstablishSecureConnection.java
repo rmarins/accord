4c33c8269a9119880b9d4519ae667d8463a76310
Rafael Marins <rafa.marins@gmail.com>
2010-08-12 21:02:37 +0000

    OFTP establish secure connection example.

---
+++ examples/src/main/java/org/neociclo/odetteftp/examples/EstablishSecureConnection.java
@@ -0,0 +1,64 @@
+/**
+ * Neociclo Accord, Open Source B2B Integration Suite
+ * Copyright (C) 2005-2010 Neociclo, http://www.neociclo.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
+package org.neociclo.odetteftp.examples;
+
+import java.net.InetSocketAddress;
+
+import javax.net.ssl.SSLEngine;
+
+import org.neociclo.odetteftp.examples.support.DefaultOftpletFactory;
+import org.neociclo.odetteftp.examples.support.SampleOftpSslContextFactory;
+import org.neociclo.odetteftp.examples.support.SessionConfig;
+import org.neociclo.odetteftp.oftplet.OftpletFactory;
+import org.neociclo.odetteftp.service.TcpClient;
+
+/**
+ * @author Rafael Marins
+ * @version $Rev$
+ */
+public class EstablishSecureConnection {
+
+	public static void main(String[] args) throws Exception {
+
+		MainSupport ms = new MainSupport(EstablishSecureConnection.class, args, "server", "port", "oid", "password");
+
+		String server = ms.get(0);
+		int port = Integer.parseInt(ms.get(1));
+		String oid = ms.get(2);
+		String password = ms.get(3);
+
+		SessionConfig conf = new SessionConfig();
+		conf.setUserCode(oid);
+		conf.setUserPassword(password);
+
+		OftpletFactory factory = new DefaultOftpletFactory(conf);
+
+		// create the client mode SSL engine
+		SSLEngine sslEngine = SampleOftpSslContextFactory.getClientContext().createSSLEngine();
+		sslEngine.setUseClientMode(true);
+		sslEngine.setEnableSessionCreation(true);
+
+		TcpClient oftp = new TcpClient(new InetSocketAddress(server, port), sslEngine, factory);
+
+		oftp.connect(true);
+
+	}
+
+}

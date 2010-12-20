a88da1e80fdc3e42ecaad2fd04efdaba96758a6f
Rafael Marins <rafa.marins@gmail.com>
2010-12-20 12:55:48 +0000

    Added X25 MoreDataBit client and server

---
+++ core/src/main/java/org/neociclo/odetteftp/service/X25MoreDataBitClient.java
@@ -0,0 +1,66 @@
+/**
+ * Neociclo Accord, Open Source B2B Integration Suite
+ * Copyright (C) 2005-2010 Neociclo, http://www.neociclo.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
+package org.neociclo.odetteftp.service;
+
+import java.net.InetSocketAddress;
+
+import javax.net.ssl.SSLEngine;
+
+import org.neociclo.odetteftp.TransportType;
+import org.neociclo.odetteftp.oftplet.OftpletFactory;
+
+/**
+ * @author Rafael Marins
+ * @version $Rev$ $Date$
+ */
+public class X25MoreDataBitClient extends TcpClient {
+
+	private static final TransportType X25_MBGW_TRANSPORT_TYPE = TransportType.X25_MBGW;
+
+	public X25MoreDataBitClient(String host, OftpletFactory oftpletFactory) {
+		super(host, oftpletFactory);
+	}
+
+	public X25MoreDataBitClient(String host, SSLEngine sslEngine, OftpletFactory oftpletFactory) {
+		super(host, sslEngine, oftpletFactory);
+	}
+
+	public X25MoreDataBitClient(String host, int port, OftpletFactory oftpletFactory) {
+		super(host, port, oftpletFactory);
+	}
+
+	public X25MoreDataBitClient(String host, int port, SSLEngine sslEngine, OftpletFactory oftpletFactory) {
+		super(host, port, sslEngine, oftpletFactory);
+	}
+
+	public X25MoreDataBitClient(InetSocketAddress remoteAddress, OftpletFactory oftpletFactory) {
+		super(remoteAddress, oftpletFactory);
+	}
+
+	public X25MoreDataBitClient(InetSocketAddress remoteAddress, SSLEngine sslEngine, OftpletFactory oftpletFactory) {
+		super(remoteAddress, sslEngine, oftpletFactory);
+	}
+	
+	@Override
+	protected TransportType getTransportType() {
+		return X25_MBGW_TRANSPORT_TYPE;
+	}
+
+}
---
+++ core/src/main/java/org/neociclo/odetteftp/service/X25MoreDataBitServer.java
@@ -0,0 +1,54 @@
+/**
+ * Neociclo Accord, Open Source B2B Integration Suite
+ * Copyright (C) 2005-2010 Neociclo, http://www.neociclo.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
+package org.neociclo.odetteftp.service;
+
+import java.net.InetSocketAddress;
+
+import javax.net.ssl.SSLEngine;
+
+import org.neociclo.odetteftp.TransportType;
+import org.neociclo.odetteftp.oftplet.OftpletFactory;
+
+/**
+ * @author Rafael Marins
+ * @version $Rev$ $Date$
+ */
+public class X25MoreDataBitServer extends TcpServer {
+
+	private static final TransportType X25_MBGW_TRANSPORT_TYPE = TransportType.X25_MBGW;
+
+	public X25MoreDataBitServer(InetSocketAddress localAddress, OftpletFactory oftpletFactory) {
+		super(localAddress, oftpletFactory);
+	}
+
+	public X25MoreDataBitServer(InetSocketAddress localAddress, SSLEngine sslEngine, OftpletFactory oftpletFactory) {
+		super(localAddress, sslEngine, oftpletFactory);
+	}
+
+	public X25MoreDataBitServer(InetSocketAddress localAddress, SSLEngine sslEngine, Boolean startTls,
+			OftpletFactory oftpletFactory) {
+		super(localAddress, sslEngine, startTls, oftpletFactory);
+	}
+
+	@Override
+	public TransportType getTransportType() {
+		return X25_MBGW_TRANSPORT_TYPE;
+	}
+}
